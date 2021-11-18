362c2bc92cef58b6e978ef73c9839504facbbbb1
Junaem <junh9547@gmail.com>
2021-11-18 10:42:55 +0900

    accounts && jwt

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from rest_framework.status import (
+    HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+)
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 # Create your views here.
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def signup(request):
+    password = request.data.get('password')
+    password_confirmation = request.data.get('password_confirmation')
+    if password !=password_confirmation:
+        return Response({'password': ['비밀번호 확인이 일치하지 않습니다.']}, HTTP_400_BAD_REQUEST)
+
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(password)
+        user.save()
+        return Response(serializer.data, HTTP_201_CREATED)
