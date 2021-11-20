5d8ab543e5d905e1b34d452865fd66065eb77603
Junaem <junh9547@gmail.com>
2021-11-20 17:13:11 +0900

    check2

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:movie_pk>/', views.detail),
-    path('like/', views.movie_like)
+    path('like/', views.movie_like),
 ]
