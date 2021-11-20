e240628446a1d7c59db5eeb4162b146a3187500e
Junaem <junh9547@gmail.com>
2021-11-20 21:28:17 +0900

    minor

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:movie_pk>/', views.detail),
-    path('<int:movie_pk>/like/', views.get_movie_like),
+    path('<int:movie_id>/like/', views.get_movie_like),
     path('like/', views.post_movie_like),
 ]
